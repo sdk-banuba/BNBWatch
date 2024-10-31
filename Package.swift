b972562bce80820aa847cdc404737f318cadba6c
ci <ci@banuba.com>
2024-10-31 13:02:05 +0300

    1.14.1-170-g93323bc96

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.1-170-g91aac4bda"
+let bnbPackageVersion: Version = "1.14.1-170-g93323bc96"
 
 let package = Package(
     name: "BNBWatch",
@@ -36,8 +36,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBWatch",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-170-g91aac4bda/BNBWatch.zip",
-            checksum: "df752742a29f1e45b18e1c353a56e090bc2a3fba20df961c6555d494affb9f8b"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-170-g93323bc96/BNBWatch.zip",
+            checksum: "587d453805f9c532b39beb6c60518aae6e28ed20ee00c34f1f7633723af83567"
         ),
         .target(
             name: "BNBWatch_BNBSdkCore",
